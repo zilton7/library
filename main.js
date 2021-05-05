6beb7af4c9cf9a0d744be343f3d1df46367abe84
Zil Norvilis <zilasino27@gmail.com>
2021-05-05 11:40:39 +0200

    Add book remove functionality

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 // Show/Hide form based on button click
-let new_book_button = document.querySelector('#new-book');
-let newButtonForm = document.querySelector('.hidden-form');
+let new_book_button = document.querySelector("#new-book");
+let newButtonForm = document.querySelector(".hidden-form");
 
-new_book_button.addEventListener('click', () => {
-  newButtonForm.classList.toggle('ninja')
-})
-
+new_book_button.addEventListener("click", () => {
+  newButtonForm.classList.toggle("ninja");
+});
 
 // logic for library
 let library = [];
@@ -39,38 +38,41 @@ const book2 = new Book("Other Book", "Johnny Johnson", 400, false);
 addBookToLibrary(book1);
 addBookToLibrary(book2);
 
-function removeBook(title){
-  console.log(title)
+function removeBook(book_id) {
+  library.splice(book_id, 1);
+  index();
 }
 
 function index() {
   let table = document.getElementById("book-table");
 
-  table.innerHTML = '<tr><th> Title</th ><th>Author</th><th>Pages</th><th>Status</th><th>Remove Book</th></tr>'
-
+  table.innerHTML =
+    "<tr><th> Title</th ><th>Author</th><th>Pages</th><th>Status</th><th>Remove Book</th></tr>";
+  let iter = 0;
   library.forEach((book) => {
-    let book_data = `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td>${book.already_read()}</td><th><a onclick="removeBook('${book.title}')" href="#" class="remove-button color-red">X</a></th></tr>`;
+    let book_data = `<tr><td>${book.title}</td><td>${book.author}</td><td>${
+      book.pages
+    }</td><td>${book.already_read()}</td><th><a onclick="removeBook(${iter});" href="#" class="remove-button color-red">X</a></th></tr>`;
     table.innerHTML += book_data;
+    iter++;
   });
-
-
-
 }
 
 index();
 
-
 // When button is clicked add a new book
-let submit_button = document.getElementById('submit-button').addEventListener('click', () => {
-  let new_title = document.getElementById('title').value
-  let new_author = document.getElementById('author').value
-  let new_num_pages = document.getElementById('num_pages').value
-  let new_checkbox = document.getElementById('read').checked
+let submit_button = document
+  .getElementById("submit-button")
+  .addEventListener("click", () => {
+    let new_title = document.getElementById("title").value;
+    let new_author = document.getElementById("author").value;
+    let new_num_pages = document.getElementById("num_pages").value;
+    let new_checkbox = document.getElementById("read").checked;
 
-  let new_book = new Book(new_title, new_author, new_num_pages, new_checkbox);
-  addBookToLibrary(new_book)
+    let new_book = new Book(new_title, new_author, new_num_pages, new_checkbox);
+    addBookToLibrary(new_book);
 
-  console.log(new_book)
+    console.log(new_book);
 
-  index();
-})
+    index();
+  });
