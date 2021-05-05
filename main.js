8f749b14db99b41b2350d8ed2a6ceffb49ec7a09
Zil Norvilis <zilasino27@gmail.com>
2021-05-05 12:33:31 +0200

    Fix linter offences

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 // Show/Hide form based on button click
-let new_book_button = document.querySelector("#new-book");
-let newButtonForm = document.querySelector(".hidden-form");
+const newBookButton = document.querySelector('#new-book');
+const newButtonForm = document.querySelector('.hidden-form');
 
-new_book_button.addEventListener("click", () => {
-  newButtonForm.classList.toggle("ninja");
+newBookButton.addEventListener('click', () => {
+  newButtonForm.classList.toggle('ninja');
 });
-
-// LocalStorage stuff
-function saveToLocalStorage() {
-  window.localStorage.setItem("library", JSON.stringify(library));
-}
 
 // logic for library
 let library = [];
-if (localStorage.getItem("library") !== null) {
-  library = JSON.parse(window.localStorage.getItem("library"));
+if (localStorage.getItem('library') !== null) {
+  library = JSON.parse(window.localStorage.getItem('library'));
+}
+
+// LocalStorage stuff
+function saveToLocalStorage() {
+  window.localStorage.setItem('library', JSON.stringify(library));
 }
 
 function Book(title, author, pages, read) {
@@ -37,13 +37,14 @@ function addBookToLibrary(obj) {
 // addBookToLibrary(book1);
 // addBookToLibrary(book2);
 
-function removeBook(book_id) {
-  library.splice(book_id, 1);
+/* eslint-disable */
+function removeBook(bookId) {
+  library.splice(bookId, 1);
   index();
 }
 
-function changeStatus(book_id) {
-  let book = library[book_id];
+function changeStatus(bookId) {
+  const book = library[bookId];
   console.log(book);
   if (book.read) {
     book.read = false;
@@ -53,16 +54,16 @@ function changeStatus(book_id) {
 
   index();
 }
+/* eslint-enable */
 
 function index() {
-  let table = document.getElementById("book-table");
-  table.innerHTML =
-    "<tr><th> Title</th ><th>Author</th><th>Pages</th><th>Read?</th><th>Remove Book</th></tr>";
+  const table = document.getElementById('book-table');
+  table.innerHTML = '<tr><th> Title</th ><th>Author</th><th>Pages</th><th>Read?</th><th>Remove Book</th></tr>';
   let iter = 0;
   library.forEach((book) => {
-    let book_data = `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td><a href="#" onclick="changeStatus(${iter});">${book.read}</a></td><th><a onclick="removeBook(${iter});" href="#" class="remove-button color-red">X</a></th></tr>`;
-    table.innerHTML += book_data;
-    iter++;
+    const bookData = `<tr><td>${book.title}</td><td>${book.author}</td><td>${book.pages}</td><td><a href="#" onclick="changeStatus(${iter});">${book.read}</a></td><th><a onclick="removeBook(${iter});" href="#" class="remove-button color-red">X</a></th></tr>`;
+    table.innerHTML += bookData;
+    iter += 1;
   });
 
   saveToLocalStorage();
@@ -71,18 +72,16 @@ function index() {
 index();
 
 // When button is clicked add a new book
-let submit_button = document
-  .getElementById("submit-button")
-  .addEventListener("click", () => {
-    let new_title = document.getElementById("title").value;
-    let new_author = document.getElementById("author").value;
-    let new_num_pages = document.getElementById("num_pages").value;
-    let new_checkbox = document.getElementById("read").checked;
+document.getElementById('submit-button').addEventListener('click', () => {
+  const newTitle = document.getElementById('title').value;
+  const newAuthor = document.getElementById('author').value;
+  const newNumPages = document.getElementById('num_pages').value;
+  const newCheckbox = document.getElementById('read').checked;
 
-    let new_book = new Book(new_title, new_author, new_num_pages, new_checkbox);
-    addBookToLibrary(new_book);
+  const newBook = new Book(newTitle, newAuthor, newNumPages, newCheckbox);
+  addBookToLibrary(newBook);
 
-    console.log(new_book);
+  console.log(newBook);
 
-    index();
-  });
+  index();
+});
